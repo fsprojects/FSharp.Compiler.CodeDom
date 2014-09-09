304adc35ade60dbbf7efa1a478333dd3c3c947b3
F# Software Foundation <fsgit@fastmail.fm>
2014-09-09 12:02:32 +0100

    update assemblyinfo.fs

--- src/FSharp.Compiler.CodeDom/AssemblyInfo.fs
+++ src/FSharp.Compiler.CodeDom/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FSharp.Compiler.CodeDom")>]
 [<assembly: AssemblyProductAttribute("FSharp.Compiler.CodeDom")>]
 [<assembly: AssemblyDescriptionAttribute("A limited CodeDom implemenation for F#")>]
-[<assembly: AssemblyVersionAttribute("1.0")>]
-[<assembly: AssemblyFileVersionAttribute("1.0")>]
+[<assembly: AssemblyVersionAttribute("0.9.0")>]
+[<assembly: AssemblyFileVersionAttribute("0.9.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.0"
+    let [<Literal>] Version = "0.9.0"
