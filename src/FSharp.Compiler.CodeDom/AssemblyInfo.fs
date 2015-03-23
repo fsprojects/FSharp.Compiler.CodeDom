3bad2d39dc37662970736d08495f706aab867e9c
7sharp9 <dave.thomas@xamarin.com>
2015-03-23 15:04:37 +0000

    Update release notes and assembly info

--- src/FSharp.Compiler.CodeDom/AssemblyInfo.fs
+++ src/FSharp.Compiler.CodeDom/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FSharp.Compiler.CodeDom")>]
 [<assembly: AssemblyProductAttribute("FSharp.Compiler.CodeDom")>]
 [<assembly: AssemblyDescriptionAttribute("A limited CodeDom implementation for F#")>]
-[<assembly: AssemblyVersionAttribute("0.9.1")>]
-[<assembly: AssemblyFileVersionAttribute("0.9.1")>]
+[<assembly: AssemblyVersionAttribute("0.9.2")>]
+[<assembly: AssemblyFileVersionAttribute("0.9.2")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.9.1"
+    let [<Literal>] Version = "0.9.2"
